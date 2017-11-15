6bbf21804770e945073de2fa01ee7e2dfffe176b
David Nyman <davenyman@gmail.com>
2017-11-15 14:32:30 -0500

    finished exercise

--- index.js
+++ index.js
@@ -2,4 +2,27 @@
   'use strict';
 
   // YOUR CODE HERE
+  var stopButton = document.getElementById('stopButton');
+  var slowButton = document.getElementById('slowButton');
+  var goButton = document.getElementById('goButton');
+
+  var stopLight = document.getElementById("stopLight")
+  var slowLight = document.getElementById("slowLight")
+  var goLight = document.getElementById("goLight")
+
+stopLight.classList.add("stop");
+slowLight.classList.add("slow");
+goLight.classList.add("go");
+
+
+  stopButton.addEventListener("click", function(){
+    stopLight.classList.toggle("stop");
+  });
+  slowButton.addEventListener("click", function(){
+    slowLight.classList.toggle("slow");
+  });
+  goButton.addEventListener("click", function(){
+    goLight.classList.toggle("go");
+  });
+
 })();
